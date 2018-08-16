e5ea3abf155f9eb39fdcfed9d978b9382a62e6c2
Kevin Ries <kevinries@Kevins-MacBook-Pro.local>
2018-08-17 00:15:41 +0200

    git cache cleared

--- node_modules/weave-repl/src/utils.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    formatNumber (value, decimals = 0, sign = false) {
-        let result = Number(value.toFixed(decimals)).toLocaleString()
-        if (sign && value > 0.0) {
-            result = '' + result
-        }
-        return result
-    }
-}
