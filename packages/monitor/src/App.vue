26e8637c9d1eb6e46f1cff643440212b8249ac63
Kevin Ries <kevinries@Kevins-iMac-Pro.fritz.box>
2018-12-15 02:47:24 +0100

    Changed style of landscape monitor

--- packages/monitor/src/App.vue
+++ packages/monitor/src/App.vue
@@ -1,22 +1,37 @@
 <template>
     <div id="app">
-    <div id="nav">
-        <router-link to="/">Nodes</router-link> |
-        <router-link to="/services">Services</router-link> |
-        <router-link to="/actions">Actions</router-link> |
-        <router-link to="/metrics">Metrics</router-link>
-    </div>
-    <router-view/>
+        <header>
+            <div>
+                <img src="@/assets/Logo.png" id="logo">
+            </div>
+            <div id="nav">
+                <router-link to="/">Nodes</router-link> |
+                <router-link to="/services">Services</router-link> |
+                <router-link to="/actions">Actions</router-link> |
+                <router-link to="/metrics">Metrics</router-link>
+            </div>
+        </header>
+        <div id="content">
+            <router-view/>
+        </div>
     </div>
 </template>
 
 <style>
-#app {
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+}
+
+#logo {
+    width: 250px;
 }
 #nav {
   padding: 30px;
@@ -28,7 +43,11 @@
 }
 
 #nav a.router-link-exact-active {
-    color: #42b983;
+    color: #008ad5;
+}
+
+#content {
+    text-align: center;
 }
 
 table {
