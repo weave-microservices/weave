8669596b78699896c7a3f5af4034a38eb10519d9
Kevin Ries <kevinries@Kevins-iMac-Pro.fritz.box>
2019-02-09 02:16:15 +0100

    fix: Service loader

--- packages/core/lib/broker/load-services.factory.js
+++ packages/core/lib/broker/load-services.factory.js
@@ -4,19 +4,17 @@
  * Copyright 2018 Fachwerk
  */
 
+// npm modules
 const path = require('path')
-const glob = require('tiny-glob')
+const glob = require('glob')
 
 const loadServicesFactory = ({ log, loadService }) =>
     (folder = './services', fileMask = '*.service.js') => {
         log.info(`Searching services in folder '${folder}' with name pattern '${fileMask}'.`)
-        return glob(path.join(folder, fileMask))
-            .then(serviceFiles => {
-                // const serviceFiles = glob.sync(path.join(folder, fileMask))
-                log.info(`${serviceFiles.length} services found.`)
-                serviceFiles.forEach(fileName => loadService(fileName))
-                return serviceFiles.length
-            })
+        const serviceFiles = glob.sync(path.join(folder, fileMask))
+        log.info(`${serviceFiles.length} services found.`)
+        serviceFiles.forEach(fileName => loadService(fileName))
+        return serviceFiles.length
     }
 
 module.exports = loadServicesFactory
