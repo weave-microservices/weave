7c53cce489000d0245f3ff6584dcb60fea45d371
Kevin Ries <kevinries@Kevins-iMac-Pro.fritz.box>
2019-03-22 18:58:01 +0100

    Added unit test for internal service

---
+++ packages/core/test/integration/internal-service.test.js
@@ -0,0 +1,18 @@
+const { Weave } = require('../../lib/index')
+
+describe('Test internal service $node', () => {
+    it('Five actions from "$node" should be available.', (done) => {
+        const node1 = Weave({
+            nodeId: 'node1',
+            logLevel: 'fatal'
+        })
+
+        node1.start().then(() => {
+            node1.call('$node.actions')
+                .then(res => {
+                    expect(res.length).toBe(5)
+                    done()
+                })
+        })
+    })
+})
