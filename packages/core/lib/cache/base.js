45a25a369ca2b7e9c1284ae9da6bd152c2cb29cd
Kevin Ries <kevinries@Kevins-iMac-Pro.local>
2019-06-03 01:55:54 +0200

    Removed unused method

--- packages/core/lib/cache/base.js
+++ packages/core/lib/cache/base.js
@@ -8,29 +8,6 @@ const { hash } = require('node-object-hash')({ sort: false, coerce: false })
 const { isObject } = require('lodash')
 
 const makeBaseCache = (broker, options) => {
-    // create a hash key from the object
-    const getCachingHash = (name, params, keys) => {
-        if (params) {
-            const prefix = `${name}:`
-            if (keys) {
-                if (keys.length === 1) {
-                    const value = params[keys[0]]
-                    return prefix + (isObject(value) ? hash(value) : value)
-                }
-                if (keys.length > 0) {
-                    const res = keys.reduce((p, key, i) => {
-                        const value = params[key]
-                        return p + (i ? '|' : '') + (isObject(value) ? hash(value) : value)
-                    }, prefix)
-                    return res
-                }
-            } else {
-                return prefix + hash(params)
-            }
-        }
-        return name
-    }
-
     const baseCache = {
         options: Object.assign({
             ttl: null
