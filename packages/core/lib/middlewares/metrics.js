0bba0308e75146790994481023f6331899512083
= <=>
2019-08-07 08:23:15 +0200

    chore: Implemented new metrics module

--- packages/core/lib/middlewares/metrics.js
+++ packages/core/lib/middlewares/metrics.js
@@ -5,9 +5,31 @@
  */
 
 module.exports = () => {
-    return {
-        created () {
-            
+    function wrapMetricMiddleware (handler, action) {
+        const broker = this
+        return function metricMiddleware (context) {
+            broker.metrics.increment('REQUEST_TOTAL')
+            broker.metrics.increment('REQUEST_IN_FLIGHT')
+
+            return handler(context)
+                .then(result => {
+                    broker.metrics.decrement('REQUEST_IN_FLIGHT')
+                    return result
+                })
+                .catch(error => {
+                    broker.metrics.decrement('REQUEST_IN_FLIGHT')
+                    broker.metrics.increment('REQUEST_ERROR_TOTAL')
+                    throw error
+                })
         }
+    }
+
+    return {
+        created (t) {
+            this.metrics.register({ type: 'counter', name: 'REQUEST_TOTAL' })
+            this.metrics.register({ type: 'gauge', name: 'REQUEST_IN_FLIGHT' })
+            this.metrics.register({ type: 'counter', name: 'REQUEST_ERROR_TOTAL' })
+        },
+        localAction: wrapMetricMiddleware
     }
 }
