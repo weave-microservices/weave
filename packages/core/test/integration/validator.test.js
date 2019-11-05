94601ce830a6640d7cecff5af4270de8674ace73
Kevin Ries <kevin.ries@fachwerk.io>
2019-11-05 17:21:52 +0100

    chore: Added Test for validator middleware

---
+++ packages/core/test/integration/validator.test.js
@@ -0,0 +1,36 @@
+const { Weave } = require('../../lib/index')
+
+describe('Test param validator', () => {
+    it('should fail with error and validation data.', (done) => {
+        const node1 = Weave({
+            nodeId: 'node1',
+            logger: {
+                enabled: false
+            }
+        })
+
+        node1.createService({
+            name: 'testService',
+            actions: {
+                sayHello: {
+                    params: {
+                        name: { type: 'string', minLength: 10 }
+                    },
+                    handler (context) {
+                        return `Hello ${context.params.name}!`
+                    }
+                }
+            }
+        })
+
+        node1.start().then(() => {
+            node1.call('testService.sayHello', { name: 'Hans' })
+                .catch(error => {
+                    expect(error.name).toBe('WeaveParameterValidationError')
+                    expect(error.message).toBe('Parameter validation error')
+                    done()
+                })
+        })
+    })
+})
+
