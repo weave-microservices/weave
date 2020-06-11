ad69e2c0343baaa9ef188ffd68c9189ffc5178e5
Kevin Ries <kevin.ries@fachwerk.io>
2020-06-11 14:03:28 +0200

    Added unit test

--- packages/utils/test/defaults.test.js
+++ packages/utils/test/defaults.test.js
@@ -49,12 +49,35 @@ describe('Get properties by dot seperated path', () => {
     expect(utils.defaultsDeep(undefined, source)).toEqual(source)
   })
 
-  it('should return a property', () => {
+  it('should not merge undefined props', () => {
     const source = {
       name: 'test',
       settings: {
         a: 100,
+        endpoints: {
+          http: true,
+          tcp: false,
+          ws: [1, 2, 3]
+        }
+      }
+    }
+
+    expect(utils.defaultsDeep(source, undefined)).toEqual(source)
+  })
+
+  it('should return a property', () => {
+    const source = {
+      name: 'test',
+      load: true,
+      adapter: undefined,
+      settings: {
+        a: 100,
         b: 400,
+        test: {
+          a: '',
+          b: 1,
+          c: undefined
+        },
         endpoints: {
           http: true,
           tcp: false,
@@ -65,6 +88,7 @@ describe('Get properties by dot seperated path', () => {
 
     const target = {
       name: 'test2',
+      load: false,
       settings: {
         a: 200,
         endpoints: {
@@ -79,9 +103,14 @@ describe('Get properties by dot seperated path', () => {
     const merged = utils.defaultsDeep(target, source)
     expect(merged).toEqual({
       name: 'test2',
+      load: false,
       settings: {
         a: 200,
         b: 400,
+        test: {
+          a: '',
+          b: 1
+        },
         endpoints: {
           https: true,
           http: false,
