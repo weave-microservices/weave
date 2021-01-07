22f7ea146c6a487a252a6830c8f205f17f32874e
Kevin Ries <kevin.ries@fachwerk.io>
2021-01-07 16:44:58 +0100

    feat: Added dot set function

---
+++ packages/core/weave-utils/lib/dot-set.js
@@ -0,0 +1,25 @@
+const { isObject } = require('./is-object')
+
+module.exports.dotSet = function dotSet (object, key, value) {
+  if (key.includes('.')) {
+    const pathArray = key.split('.')
+    return pathArray.reduce((obj, i, index) => {
+      const isTargetProp = (index + 1) === pathArray.length
+      const currentIsOject = isObject(obj[i])
+
+      if (obj[i] === undefined && !isTargetProp) {
+        obj[i] = {}
+      } else if (!isTargetProp && currentIsOject) {
+        return obj[i]
+      } else if (isTargetProp) {
+        obj[i] = value
+      } else {
+        throw new Error(`The property "${i}" already exists and is not an object.`)
+      }
+      return obj[i]
+    }, object)
+  }
+
+  object[key] = value
+  return object
+}
