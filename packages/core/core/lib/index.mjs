84d20566827c3112b1a0d3293a4bc9afc644687b
Kevin Ries <kevin.ries@fachwerk.io>
2022-02-20 21:36:22 +0100

    feat: added support for ESM

---
+++ packages/core/core/lib/index.mjs
@@ -0,0 +1,15 @@
+import module from './index.js'
+
+export default module
+
+export const defaultOptions = module.defaultOptions
+export const createBroker = module.createBroker
+export const Errors = module.Errors
+export const Constants = module.Constants
+export const TracingAdapters = module.TracingAdapters
+export const TransportAdapters = module.TransportAdapters
+export const Cache = module.Cache
+export const createBaseTracingCollector = module.createBaseTracingCollector
+export const defineBrokerOptions = module.defineBrokerOptions
+export const defineService = module.defineService
+export const defineAction = module.defineAction
