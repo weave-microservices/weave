c5889f55fc70878a10946057a28acb025638c135
Kevin Ries <kevin.ries@fachwerk.io>
2022-05-10 10:49:36 +0200

    feat(core): Added error handler for events

--- packages/core/core/lib/middlewares/error-handler/index.js
+++ packages/core/core/lib/middlewares/error-handler/index.js
@@ -19,17 +19,33 @@ module.exports = (runtime) => {
             runtime.transport.removePendingRequestsById(context.id);
           }
 
-          runtime.log.debug(`The action ${context.action.name} is rejected`, { requestId: context.id }, error);
+          runtime.log.debug(`The action "${context.action.name}" was rejected`, { requestId: context.requestId }, error);
           return runtime.handleError(error);
         });
-      // .catch(error => {
-      //   runtime.log.error(error)
-      // })
+    };
+  };
+
+  const wrapEventErrorHandlerMiddleware = function (handler) {
+    return function errorHandlerMiddleware (context, serviceInjections) {
+      return handler(context, serviceInjections)
+        .catch(error => {
+          if (!(error instanceof Error)) {
+            error = new WeaveError(error, 500);
+          }
+
+          if (runtime.nodeId !== context.nodeId) {
+            runtime.transport.removePendingRequestsById(context.id);
+          }
+
+          runtime.log.debug(`The event "${context.eventName}" was rejected`, { requestId: context.requestId }, error);
+          return runtime.handleError(error);
+        });
     };
   };
 
   return {
     localAction: wrapErrorHandlerMiddleware,
-    remoteAction: wrapErrorHandlerMiddleware
+    remoteAction: wrapErrorHandlerMiddleware,
+    localEvent: wrapEventErrorHandlerMiddleware
   };
 };
