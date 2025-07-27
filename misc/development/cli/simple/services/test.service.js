66711d31d2c8cd5087c2289f1a7953ffba04ab22
Kevin Ries <kevin.ries@fachwerk.io>
2025-07-27 15:17:26 +0200

    removed double started hook

--- misc/development/cli/simple/services/test.service.js
+++ misc/development/cli/simple/services/test.service.js
@@ -4,16 +4,16 @@ exports.name = 'test-service';
 
 exports.started = function () {
   this.timer = setInterval(() => {
+    testFunc();
   }, 2000);
 };
 
 exports.actions = {
   hello (context) {
     console.log(context.data);
+    // Test that requestId appears in service logs
+    this.log.info('Testing requestId in logs', { data: context.data });
     return context.data;
   }
 };
 
-exports.started = async function () {
-  testFunc();
-};
